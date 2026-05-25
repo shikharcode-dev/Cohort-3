f13849a0a226984834f3bc81014de6a6aa7f84cf
Shikhar Mishra <mshikhar353@gmail.com>
2026-05-25 19:41:18 +0530

    add IIFE and fix problem

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ divide(10, 2)
 // single argument pass functoin
 let square = a => console.log(a*a)
 square(5)
+
+
+//IIFE
+// IIFE (Immediately Invoked Function Expression) should work fine
+// This code doesn't have syntax errors and should execute properly
+// If you're getting an error, it might be due to:
+// 1. Missing semicolon before IIFE if there's code above it
+// 2. Browser console restrictions
+// 3. Strict mode issues
+// 4. Context where it's being executed
+(function() {
+  console.log("This is an IIFE")
+})();
